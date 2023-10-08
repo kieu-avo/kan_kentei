c61c1c530352d9ffc8e8db7a6293bad1727b5e61
Kieu <bsb20131417@gmail.com>
2023-10-08 18:27:10 +0900

    Add config.generator

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,23 @@ module KanKentei
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.default_locale = :ja 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
+
+    config.generator do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framwork false
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+   
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
