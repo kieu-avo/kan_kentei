44b411da04b920aa50ff34713898af6fd0bc76c7
Kieu <bsb20131417@gmail.com>
2023-10-12 08:47:55 +0900

    Add font and screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,24 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        yomogi: ['Yomogi', 'cursive'],
+      },
+      colors: {
+        header_footer_color: {
+          400: '#2E97A7',
+        },
+      }
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
+  },
   plugins: [require("daisyui")]
 }
