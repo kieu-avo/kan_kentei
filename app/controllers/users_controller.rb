f3db9ea9d78a985e4f72b4016d834b1d05236d75
Kieu <bsb20131417@gmail.com>
2023-10-12 17:27:24 +0900

    First commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      redirect_to login_path, success: t('.success')
     else
+      flash.now[:error] = t('.fail')
       render :new, status: :unprocessable_entity
     end
   end
