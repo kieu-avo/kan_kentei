2c58dfcf56fd7137e4adca1e76b9287a70e39807
Kieu <bsb20131417@gmail.com>
2023-10-12 10:19:47 +0900

    First commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+
+end
