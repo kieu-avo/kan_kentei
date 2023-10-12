9b28b67ba573ec8ac0468ee08fd43ce4ea09fb70
Kieu <bsb20131417@gmail.com>
2023-10-12 17:27:05 +0900

    First commit

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,14 +5,16 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to  root_path
+      redirect_back_or_to root_path, success: t('.success')
     else
+      flash.now[:error] = t('.fail')
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
+    flash[:success] = t('.logout')
     redirect_to root_path, status: :see_other
   end
 end
