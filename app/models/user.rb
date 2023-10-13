acb1a26ed8959a78751fe51741fee18d8b7a8c13
Kieu <bsb20131417@gmail.com>
2023-10-13 21:59:40 +0900

    Add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  mount_uploader :avatar, AvatarUploader
+
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
-
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
