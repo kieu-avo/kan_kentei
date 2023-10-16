9d1e7ccd5fa69aad3fb32ba600ef6409120c6fff
Kieu <bsb20131417@gmail.com>
2023-10-16 11:52:47 +0900

    Add maps

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
+  get 'maps', to: 'maps#index'
+
   resources :users, only: %i[new create]
   resource :profile, only: %i[show edit update]
 end
