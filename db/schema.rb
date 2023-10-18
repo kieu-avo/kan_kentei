4bfe0ceede218a44413600b1e5c73e07d9e91ede
Kieu <bsb20131417@gmail.com>
2023-10-18 10:57:31 +0900

    Add some tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_121521) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_18_012912) do
   create_table "japan_and_overseas_test_categories", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "quiz_choices", force: :cascade do |t|
+    t.integer "quiz_id", null: false
+    t.text "content", null: false
+    t.boolean "correct_answer", default: false, null: false
+    t.text "explanation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quiz_id"], name: "index_quiz_choices_on_quiz_id"
+  end
+
+  create_table "quizzes", force: :cascade do |t|
+    t.integer "japan_and_overseas_test_category_id", null: false
+    t.text "content", null: false
+    t.string "youtube_url"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["japan_and_overseas_test_category_id"], name: "index_quizzes_on_japan_and_overseas_test_category_id"
+  end
+
+  create_table "user_quiz_answers", force: :cascade do |t|
+    t.integer "quiz_id", null: false
+    t.integer "quiz_choice_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quiz_choice_id"], name: "index_user_quiz_answers_on_quiz_choice_id"
+    t.index ["quiz_id"], name: "index_user_quiz_answers_on_quiz_id"
+    t.index ["user_id"], name: "index_user_quiz_answers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +59,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_17_121521) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "quiz_choices", "quizzes"
+  add_foreign_key "quizzes", "japan_and_overseas_test_categories"
+  add_foreign_key "user_quiz_answers", "quiz_choices"
+  add_foreign_key "user_quiz_answers", "quizzes"
+  add_foreign_key "user_quiz_answers", "users"
 end
