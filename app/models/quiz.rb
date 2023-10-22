f9eaaea30ef8e05f085b8134a743b56cc9ec02b2
Kieu <bsb20131417@gmail.com>
2023-10-22 10:29:14 +0900

    Delete unused method

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,9 +1,4 @@
 class Quiz < ApplicationRecord
   belongs_to :japan_and_overseas_test_category
   has_many :quiz_choices
-
-  scope :select_category, ->(name) {
-    category = JapanAndOverseasTestCategory.find_by(title: name)
-    includes(:quiz_choices).where(japan_and_overseas_test_category_id: category.id)
-    }
 end
