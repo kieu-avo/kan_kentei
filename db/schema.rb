7739b1baad0eb550fec470e6fa2a3e1fba5e34af
Kieu <bsb20131417@gmail.com>
2023-10-23 22:10:07 +0900

    Update tables related to quiz

--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_20_061233) do
-  create_table "japan_and_overseas_test_categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_10_22_111945) do
   create_table "quiz_choices", force: :cascade do |t|
     t.integer "quiz_id", null: false
     t.text "content", null: false
     t.boolean "correct_answer", default: false, null: false
     t.text "explanation"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_url"
     t.index ["quiz_id"], name: "index_quiz_choices_on_quiz_id"
   end
 
   create_table "quiz_results", force: :cascade do |t|
-    t.integer "japan_and_overseas_test_category_id", null: false
+    t.integer "test_category_id", null: false
     t.integer "user_id", null: false
     t.integer "score", null: false
-    t.boolean "result", default: false, null: false
+    t.boolean "is_passed", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["japan_and_overseas_test_category_id"], name: "index_quiz_results_on_japan_and_overseas_test_category_id"
+    t.index ["test_category_id"], name: "index_quiz_results_on_test_category_id"
     t.index ["user_id"], name: "index_quiz_results_on_user_id"
   end
 
   create_table "quizzes", force: :cascade do |t|
-    t.integer "japan_and_overseas_test_category_id", null: false
+    t.integer "test_category_id", null: false
     t.text "content", null: false
     t.string "youtube_url"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["japan_and_overseas_test_category_id"], name: "index_quizzes_on_japan_and_overseas_test_category_id"
+    t.index ["test_category_id"], name: "index_quizzes_on_test_category_id"
+  end
+
+  create_table "test_categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_quiz_answers", force: :cascade do |t|
     t.integer "quiz_id", null: false
-    t.integer "quiz_choice_id", null: false
     t.integer "user_id", null: false
+    t.integer "quiz_choice_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["quiz_choice_id"], name: "index_user_quiz_answers_on_quiz_choice_id"
@@ -72,9 +72,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_20_061233) do
   end
 
   add_foreign_key "quiz_choices", "quizzes"
-  add_foreign_key "quiz_results", "japan_and_overseas_test_categories"
+  add_foreign_key "quiz_results", "test_categories"
   add_foreign_key "quiz_results", "users"
-  add_foreign_key "quizzes", "japan_and_overseas_test_categories"
+  add_foreign_key "quizzes", "test_categories"
   add_foreign_key "user_quiz_answers", "quiz_choices"
   add_foreign_key "user_quiz_answers", "quizzes"
   add_foreign_key "user_quiz_answers", "users"
