007bcae1da960702451601a4407512dfa49e5baa
Kieu <bsb20131417@gmail.com>
2023-10-27 22:36:46 +0900

    Add dependent: :destroy

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,5 @@
 class Quiz < ApplicationRecord
   belongs_to :test_category
-  has_many :quiz_choices
-  has_many :user_quiz_answers
+  has_many :quiz_choices, dependent: :destroy
+  has_many :user_quiz_answers, dependent: :destroy
 end
