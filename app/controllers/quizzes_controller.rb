5b4e73193b2ae4e2e40cd8ff80fef2f645517900
Kieu <bsb20131417@gmail.com>
2023-10-29 21:47:18 +0900

    　Add handling for user's unanswered questions

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,52 +1,54 @@
 class QuizzesController < ApplicationController
+  before_action :set_category, only: %i[index calculate_score show]
+  before_action :set_quiz, only: %i[index calculate_score show]
 
-  def index
-    @category = TestCategory.find(params[:category_id])
-    @quizzes = @category.quizzes.includes(:quiz_choices)
-  end
-  
+  def index; end
+
   def calculate_score
-    answers = params[:answers]
+    answers = params[:answers] || {}
     correct_count = 0
+    count_quizzes = Quiz.total_by_category(@category.id)
 
-    #正解の選択肢を取得
+    # 未選択の問題があった場合の処理
+    if answers.empty? || answers.keys.size < count_quizzes
+      flash.now[:error] = t('.blank')
+      @user_answers = answers
+      render :index, status: :unprocessable_entity and return
+    end
+
+    # 正解の選択肢を取得
     correct_choices = QuizChoice.where(quiz_id: answers.keys, correct_answer: true).index_by(&:quiz_id)
 
-    #正解数を計算
+    # 正解数を計算
     answers.each do |quiz, choice|
-      if correct_choices[quiz.to_i]&.id.to_s == choice
-        correct_count += 1
-      end
-      #ユーザーが選んだ回答をUserQuizAnswerに保存
+      correct_count += 1 if correct_choices[quiz.to_i]&.id.to_s == choice
+      # ユーザーが選んだ回答をUserQuizAnswerに保存
       UserQuizAnswer.create(
-        user_id: current_user.id, 
-        quiz_id: quiz, 
+        user_id: current_user.id,
+        quiz_id: quiz,
         quiz_choice_id: choice
       )
     end
 
-    #点数を計算
+    # 点数を計算
     total_quizzes = answers.keys.length
     total_score = (correct_count.to_f * 100) / total_quizzes
-    
-    #QuizResultに保存
+
+    # QuizResultに保存
     quiz_result = QuizResult.create(
       test_category_id: params[:category_id],
       user_id: current_user.id,
       score: total_score,
       is_passed: total_score >= 70
     )
-    
-    if quiz_result.save
-      redirect_to category_quiz_path(category_id: params[:category_id], id: quiz_result.id)
-    else
-      flash.now[:error] = t('.take_test_again')
-      render :index, status: :unprocessable_entity
-    end
+
+    return unless quiz_result.save
+
+    redirect_to category_quiz_path(category_id: params[:category_id], id: quiz_result.id)
   end
 
-  #クイズの結果を表示
-  def show 
+  # クイズの結果を表示
+  def show
     @quiz_result = current_user.quiz_results.find(params[:id])
     @pass_status = @quiz_result.is_passed ? t('.passed') : t('.failed')
     @category = TestCategory.find(@quiz_result.test_category_id)
@@ -55,7 +57,14 @@ class QuizzesController < ApplicationController
     @user_quiz_answers = UserQuizAnswer.where(user_id: current_user.id, quiz_id: @quizzes.pluck(:id)).index_by(&:quiz_id)
     @explanations = QuizChoice.where(quiz_id: @quizzes.pluck(:id), correct_answer: true).index_by(&:quiz_id)
   end
+
+  private
+
+  def set_category
+    @category = TestCategory.find(params[:category_id])
+  end
+
+  def set_quiz
+    @quizzes = @category.quizzes.includes(:quiz_choices)
+  end
 end
-  
-
-
