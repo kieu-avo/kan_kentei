5fbae2f57429c9726d70e3506aba1a3057a8a747
Kieu <bsb20131417@gmail.com>
2023-10-29 21:52:21 +0900

    Add method to count quizzes

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,11 @@ class Quiz < ApplicationRecord
   belongs_to :test_category
   has_many :quiz_choices, dependent: :destroy
   has_many :user_quiz_answers, dependent: :destroy
+
+  def self.total_by_category(category_id)
+    category = TestCategory.find_by(id: category_id)
+    return 0 unless category
+
+    where(test_category_id: category.id).count
+  end
 end
