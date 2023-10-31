aa6a10ad9b791d87158bd6dd64d7387c490cc304
Kieu <bsb20131417@gmail.com>
2023-10-31 22:27:13 +0900

    Associate with souvenir_photos

--- app/models/test_category.rb
+++ app/models/test_category.rb
@@ -1,4 +1,5 @@
 class TestCategory < ApplicationRecord
   has_many :quizzes, dependent: :destroy
   has_many :quiz_results, dependent: :destroy
+  has_many :souvenir_photos, dependent: :destroy
 end
