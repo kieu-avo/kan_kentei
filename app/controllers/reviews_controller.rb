f753acf29c92188de60183f5d087d902be0c6386
Kieu <bsb20131417@gmail.com>
2023-11-01 21:37:54 +0900

    Add code to get reviews from users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,27 @@
 class ReviewsController < ApplicationController
   before_action :set_category, only: %i[index new create]
-  
+  before_action :set_review, only: %i[index new create]
+
   def new
-    @reviews = Review.where(test_category: @category)
     @user_review_answer = UserReviewAnswer.new
+  end
+
+  #ユーザーからの回答をDBに保存
+  def create 
+    user_review_params[:rating].each do |review, rating|
+      @user_review_answer = UserReviewAnswer.new(
+        rating: rating.to_f,
+        user_id: current_user.id,
+        review_id: review
+      )
+
+      unless @user_review_answer.save
+        flash.now[:error] = t('.blank')
+        render :new, status: :unprocessable_entity and return
+      end
+    end
+
+    redirect_to :new_category_comment_path
   end
 
   private
@@ -12,5 +30,12 @@ class ReviewsController < ApplicationController
     @category = TestCategory.find(params[:category_id])
   end
 
-  
+  def set_review
+    @reviews = Review.where(test_category: @category)
+  end
+
+  def user_review_params
+    params.require(:user_review_answer).permit(:user_id, :review_id, rating: {})
+  end
+
 end
