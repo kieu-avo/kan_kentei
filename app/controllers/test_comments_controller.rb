76618eaaee29704c0ff14823af729f443c4acb01
Kieu <bsb20131417@gmail.com>
2023-11-02 21:58:20 +0900

    Add code to create comment page

--- app/controllers/test_comments_controller.rb
+++ app/controllers/test_comments_controller.rb
@@ -1,2 +1,31 @@
 class TestCommentsController < ApplicationController
+  before_action :set_category, only: %i[index new create show]
+
+  def index
+    @test_comment = TestComment.all
+  end
+
+  def new
+    @test_comment = TestComment.new
+    @souvenir = SouvenirPhoto.where(test_category_id: @category.id).order("RANDOM()").first
+  end
+
+  def create
+    @test_comment = TestComment.new(test_comment_params)
+    if @test_comment.save
+      redirect_to category_test_comments_path(@category), success = "コメントを投稿しました。"
+    else
+      puts @test_comment.errors.full_messages
+    end
+  end
+
+  private
+
+  def test_comment_params
+    params.require(:test_comment).permit(:content, :souvenir_photo_id, :test_category_id, :user_id)
+  end
+
+  def set_category
+    @category = TestCategory.find(params[:category_id])
+  end
 end
