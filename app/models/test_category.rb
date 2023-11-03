183ad4e62fe30539853d9a31b48fd2e721bcdf83
Kieu <bsb20131417@gmail.com>
2023-11-03 22:54:09 +0900

    Update association

--- app/models/test_category.rb
+++ app/models/test_category.rb
@@ -2,4 +2,5 @@ class TestCategory < ApplicationRecord
   has_many :quizzes, dependent: :destroy
   has_many :quiz_results, dependent: :destroy
   has_many :souvenir_photos, dependent: :destroy
+  has_many :test_comments, dependent: :destroy
 end
