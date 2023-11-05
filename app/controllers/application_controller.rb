4ef2b8ae657147368a1ae68333a9704dcef4e451
Kieu <bsb20131417@gmail.com>
2023-11-05 16:58:34 +0900

    Add require_login and not_authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
   add_flash_types :success, :error
+
+  def not_authenticated
+   redirect_to login_path, error: t('defaults.require_login')
+  end
+
+
 end
