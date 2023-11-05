9f2d8b8bf6364fd4bc9437e41ff6c71ae31e2ce8
Kieu <bsb20131417@gmail.com>
2023-11-05 22:35:45 +0900

    Add codes for index

--- app/javascript/reviews.js
+++ app/javascript/reviews.js
@@ -6,16 +6,20 @@ window.raty = function(elem, opt) {
   return raty;
 }
 
+//newとindex（平均）に星を表示
 document.addEventListener('turbo:load', function() {
   $('.review-item').each(function() {
     var reviewId = $(this).data('review-id');
     let elem = document.querySelector('#star-rating-' + reviewId);
     if (elem) {
+      let isReadyOnly = elem.hasAttribute('data-score');
       let opt = {
         starOn: elem.dataset.starOn,
         starOff: elem.dataset.starOff,
         starHalf: elem.dataset.starHalf,
-        scoreName: 'user_review_answer[rating][' + reviewId + ']'
+        scoreName: isReadyOnly ? undefined : 'user_review_answer[rating][' + reviewId + ']',
+        score: isReadyOnly ? elem.dataset.socore : undefined,
+        readOnly: isReadyOnly
       };
       window.raty(elem, opt);
     }
