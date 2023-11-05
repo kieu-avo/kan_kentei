0e7f3c1570cc5f4147c3866100be060525c2d958
Kieu <bsb20131417@gmail.com>
2023-11-05 17:10:04 +0900

    Add update method and update codes etc.

--- app/controllers/test_comments_controller.rb
+++ app/controllers/test_comments_controller.rb
@@ -1,8 +1,9 @@
 class TestCommentsController < ApplicationController
-  before_action :set_category, only: %i[index new create show edit]
+  before_action :set_category, only: %i[index new create show edit update]
 
   def index
-    @test_comments = TestComment.all.includes(:user, :test_category, :souvenir_photo).order(created_at: :desc)
+    @test_comments = @category.test_comments.includes(:user, :souvenir_photo).order(created_at: :desc)
+    # @test_comments = TestComment.all.includes(:user, :test_category, :souvenir_photo).order(created_at: :desc)
     @souvenir = SouvenirPhoto.where(test_category_id: @category.id)
   end
 
@@ -14,18 +15,37 @@ class TestCommentsController < ApplicationController
   def create
     @test_comment = current_user.test_comments.build(test_comment_params)
     if @test_comment.save
+      # redirect_to category_test_comment_path(@category, @test_comment), success: "コメントを投稿しました"
       redirect_to category_test_comments_path(@category), success: "コメントを投稿しました。"
+    else
+      @souvenir = SouvenirPhoto.find(test_comment_params[:souvenir_photo_id])
+      flash.now[:error] = "投稿に失敗しました"
+      render :new, status: :unprocessable_entity
     end
   end
 
   def show
-    @test_comment = TestComment.find(params[:id])
-    #@souvenir = SouvenirPhoto.where(test_category_id: @category.id)
+    @test_comment = current_user.test_comments.find(params[:id])
+    @souvenir = @test_comment.souvenir_photo
+    # @souvenir = @test_comment.souvenir_photo
   end
 
   def edit
-    @test_comment = TestComment.find(params[:id])
-    #@souvenir = @test_comment.souvenir_photo
+    @test_comment = current_user.test_comments.find(params[:id])
+    # @test_comment = TestComment.find(params[:id])
+    @souvenir = @test_comment.souvenir_photo
+  end
+
+  def update
+    @test_comment = current_user.test_comments.find(params[:id])
+    @souvenir = @test_comment.souvenir_photo
+
+    if @test_comment.update(test_comment_params)
+      redirect_to category_test_comment_path(@category, @test_comment), success: "コメントを更新しました"
+    else
+      flash.now[:error] = "更新を失敗しました。"
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
