83ce0a8c99cf0b7d32c1c5a492932e0bd93c2768
Kieu <bsb20131417@gmail.com>
2023-11-05 17:01:27 +0900

    Add latest_test_comments variable

--- app/controllers/passed_lists_controller.rb
+++ app/controllers/passed_lists_controller.rb
@@ -1,7 +1,6 @@
 class PassedListsController < ApplicationController
-
   def index
     @passed_quizzes = current_user.quiz_results.includes(:test_category).where(is_passed: true)
+    @latest_test_comments = current_user.test_comments.where(test_category: @passed_quizzes.map(&:test_category)).order(created_at: :desc).distinct.pluck(:test_category_id, :id).to_h
   end
-  
 end
