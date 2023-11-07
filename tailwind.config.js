271004613a875c7e4a9baa1f98bde03507f56ce2
Kieu <bsb20131417@gmail.com>
2023-11-07 19:57:44 +0900

    Fix typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-       'sans': ['ui-sans-serif', 'system-ui'],
+        'sans': ['ui-sans-serif', 'system-ui'],
         'yomogi': ['Yomogi', 'cursive'],
       },
       colors: {
