db5a616662ddd18afa8f5b29567b5d1414b7b958
Kieu <bsb20131417@gmail.com>
2023-11-08 22:39:22 +0900

    Add method to use ransack

--- app/models/test_category.rb
+++ app/models/test_category.rb
@@ -4,4 +4,8 @@ class TestCategory < ApplicationRecord
   has_many :souvenir_photos, dependent: :destroy
   has_many :test_comments, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["title"]
+  end
 end
