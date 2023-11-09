95585f7be2160b97949caefa7b82a851ad673b28
Kieu <bsb20131417@gmail.com>
2023-11-09 18:12:19 +0900

    Add validations

--- app/models/user_review_answer.rb
+++ app/models/user_review_answer.rb
@@ -2,8 +2,6 @@ class UserReviewAnswer < ApplicationRecord
   belongs_to :user
   belongs_to :review
 
-  #↓レビュー画面が完成したらコメントアウトを外す
-  #validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-  #validates :user_id, uniqueness: { scope: :review_id }
-
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :user_id, uniqueness: { scope: :review_id }
 end
