c3cf551bcfbb52e259ed5d96a54f4092d0c9ebff
Kieu <bsb20131417@gmail.com>
2023-11-09 22:29:46 +0900

    Update code to avoid complex

--- app/controllers/passed_lists_controller.rb
+++ app/controllers/passed_lists_controller.rb
@@ -1,6 +1,10 @@
 class PassedListsController < ApplicationController
   def index
-    @passed_quizzes = current_user.passed_quizzes
-    @latest_test_comments = current_user.latest_test_comments
+    #@passed_quizzes = current_user.passed_quizzes
+    #@latest_test_comments = current_user.latest_test_comments
+
+      @passed_quizzes = current_user.passed_quizzes
+      @latest_test_comments = current_user.test_comments
+
   end
 end
