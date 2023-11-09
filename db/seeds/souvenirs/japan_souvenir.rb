42c7b56b55264a0f62035e0a62175a5ce812c750
Kieu <bsb20131417@gmail.com>
2023-11-09 18:13:46 +0900

    Delete unused file

--- db/seeds/souvenirs/japan_souvenir.rb
+++
@@ -1,5 +0,0 @@
-TestCategory.all.each do |test_category|
-  Review.create!(test_category_id: test_category.id, content: "①検定は難しかったですか？")
-  Review.create!(test_category_id: test_category.id, content: "②同じ級で違う問題もチャレンジしてみたいですか？")
-  Review.create!(test_category_id: test_category.id, content: "③合格した級に行ってみたいですか？")
-end
