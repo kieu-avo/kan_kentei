00784eca9e1eba19f9a828f37ec18559ff12c31c
Kieu <bsb20131417@gmail.com>
2023-11-10 16:35:50 +0900

    Chan into

--- app/models/user_review_answer.rb
+++ app/models/user_review_answer.rb
@@ -2,6 +2,6 @@ class UserReviewAnswer < ApplicationRecord
   belongs_to :user
   belongs_to :review
 
-  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :user_id, uniqueness: { scope: :review_id }
 end
