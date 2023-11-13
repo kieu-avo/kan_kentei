d5ef58896093766373cfb18c2e0e3efd988bfbf4
Kieu <bsb20131417@gmail.com>
2023-11-13 22:14:38 +0900

    Change lenght of words in content

--- app/models/test_comment.rb
+++ app/models/test_comment.rb
@@ -3,5 +3,5 @@ class TestComment < ApplicationRecord
   belongs_to :test_category
   belongs_to :souvenir_photo
 
-  validates :content, presence: true, length: { maximum: 150 }
+  validates :content, presence: true, length: { maximum: 48 }
 end
