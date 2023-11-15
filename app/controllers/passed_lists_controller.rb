27bad9e8c03c7a418ba1ada7c67c6b78d339dcfa
Kieu <bsb20131417@gmail.com>
2023-11-15 15:21:56 +0900

    Remove unused code

--- app/controllers/passed_lists_controller.rb
+++ app/controllers/passed_lists_controller.rb
@@ -1,6 +1,6 @@
 class PassedListsController < ApplicationController
   def index
     @passed_quizzes = current_user.passed_quizzes
-    @latest_test_comments = current_user.test_comments
+    @test_comments = current_user.test_comments
   end
 end
