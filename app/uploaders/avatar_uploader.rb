e1fe09123e8e28dd214639b9721a3ebfb84ed99a
Kieu <bsb20131417@gmail.com>
2023-11-16 12:03:12 +0900

    Set up to use MiniMagic

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,8 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_fit: [150, 150]
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.production?
