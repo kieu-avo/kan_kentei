86e3d3d8383b0682c89334cfe283cf2052005e05
Kieu <bsb20131417@gmail.com>
2023-11-19 22:42:42 +0900

    Update quiz

--- db/seeds/quizzes/foreign/vietnam_quiz.rb
+++ db/seeds/quizzes/foreign/vietnam_quiz.rb
@@ -2,14 +2,14 @@ vietnam = TestCategory.create!(title: "ベトナム", region: "foreign")
 
 #---------問題１--------------------------------------------------------------------
 quiz1 = Quiz.create!(
-  content: "①今飛行機でベトナムの首都に向かっています。首都の名前は?",
+  content: "①日本で友達になったベトナム人の友人に会うために、ベトナムの首都へ向かっています。初めてのベトナム旅行で少し緊張していますが、10年ぶりに友人に会えるのがとても楽しみです！さて、その首都はどこでしょうか？",
   youtube_url: nil,
   image_url: nil,
   test_category: vietnam
 )
 
 QuizChoice.create!(
-  content: "ホーチミン",
+  content: "ホーチミン(Hồ Chí Minh)",
   correct_answer: false,
   explanation: nil,
   image_url: nil,
@@ -17,7 +17,7 @@ QuizChoice.create!(
 )
 
 QuizChoice.create!(
-  content: "ハノイ",
+  content: "ハノイ(Hà Nội)",
   correct_answer: true,
   explanation: "よくホーチミンと勘違いされますが、首都はハノイです。ホーチミンは南部に位置し、ハノイは北部に位置しています。",
   image_url: "https://kan-kentei-bucket.s3.ap-northeast-1.amazonaws.com/vietnam/q1_exp.png",
@@ -25,7 +25,7 @@ QuizChoice.create!(
 )
 
 QuizChoice.create!(
-  content: "ダナン",
+  content: "ダナン(Đà Nẵng)",
   correct_answer: false,
   explanation: nil,
   image_url: nil,
@@ -34,7 +34,7 @@ QuizChoice.create!(
 
 #---------問題２--------------------------------------------------------------------
 quiz2 = Quiz.create!(
-  content: "②ベトナム人の友達の家に遊びに行きました。明日はその友達の誕生日なので、あるものをプレゼントしたら微妙な反応をされました。そのプレゼントとは？",
+  content: "②無事に友人宅に到着しました。明日はその友達の誕生日なので、あるものをプレゼントしたらびっくりされました。その反応は嬉しいものではなく、ショックな反応でした。そのプレゼントとは何でしょう？",
   youtube_url: nil,
   image_url: nil,
   test_category: vietnam
@@ -43,8 +43,7 @@ quiz2 = Quiz.create!(
 QuizChoice.create!(
   content: "ハンカチ",
   correct_answer: true,
-  explanation: "ベトナム語でハンカチ（khăn）は「困難（khó khăn）」と発音が似ているため、ハンカチをプレゼントすると喜ばれないことがあります。\n
-              他にも避けるべきものがありますので、ベトナム人にプレゼントを贈る際は注意が必要です！",
+  explanation: "ベトナム語でハンカチ（khăn）は「困難（khó khăn）」と発音が似ています。このため、ハンカチを贈ることは、その人の未来の涙や苦しみを予見していると解釈されることがあり、喜ばれない場合があります。その他にも避けるべきプレゼントがあるため、ベトナム人への贈り物を選ぶ際には注意が必要です！",
   image_url: nil,
   quiz: quiz2
 )
@@ -67,7 +66,7 @@ QuizChoice.create!(
 
 #---------問題３--------------------------------------------------------------------
 quiz3 = Quiz.create!(
-  content: "③その友達には生後3ヶ月の赤ちゃんがいます。赤ちゃんに向かって「かわいい〜」と褒めたら友達に怒られました。どうして？",
+  content: "③その友達には生後3ヶ月の赤ちゃんがいます。とてもニコニコしていて可愛かったので、「かわいい〜」と言って赤ちゃんを褒めたら、友達から注意されました。これはなぜでしょうか？",
   youtube_url: nil,
   image_url: nil,
   test_category: vietnam
@@ -84,7 +83,7 @@ QuizChoice.create!(
 QuizChoice.create!(
   content: "褒めると赤ちゃんは不細工になるから",
   correct_answer: true,
-  explanation: "ベトナムには、赤ちゃんを褒めると将来その通りにならないという迷信があります。褒めたい時は反対の言葉を使います。",
+  explanation: "ベトナムには、赤ちゃんを褒めるとその子が将来その通りにならないという迷信があります。そのため、褒めたいときは反対の言葉を使うことが一般的です。ベトナムの赤ちゃんを褒める時は緊張しますね...!",
   image_url: nil,
   quiz: quiz3
 )
@@ -100,7 +99,7 @@ QuizChoice.create!(
 
 #---------問題４--------------------------------------------------------------------
 quiz4 = Quiz.create!(
-  content: "④赤ちゃん以外に、その友達には4歳の子供がいます。とてもいい子なので頭を撫でました。そうしたらまた友達に怒られました。どうして？",
+  content: "④赤ちゃん以外に、その友達には4歳の子供がいます。とてもいい子なので頭を撫でました。そうしたらまた友達に注意されました！どうしてですか？",
   youtube_url: nil,
   image_url: nil,
   test_category: vietnam
@@ -117,7 +116,7 @@ QuizChoice.create!(
 QuizChoice.create!(
   content: "頭は神聖な部位だから",
   correct_answer: true,
-  explanation: "人の頭を触ることは失礼な行為です。これは子供の頭に限らず大人も同様です。気をつけましょう！",
+  explanation: "赤ちゃん以外に、その友達には4歳の子供がいます。とてもいい子なので頭を撫でました。そうしたらまた友達に注意されました！どうしてですか？",
   image_url: nil,
   quiz: quiz4
 )
@@ -132,7 +131,7 @@ QuizChoice.create!(
 
 #---------問題５--------------------------------------------------------------------
 quiz5 = Quiz.create!(
-  content: "⑤友達がベトナムの家庭料理を作ってくれました。出された魚介スープの中にあるフルーツが入っていて、あなたはとても驚いています。そのフルーツとは？",
+  content: "⑤友達がベトナムの家庭料理を作ってくれました。出された魚介スープの中にあるフルーツが入っていて、あなたはとても驚いています。そのフルーツとは何ですか？",
   youtube_url: nil,
   image_url: nil,
   test_category: vietnam
@@ -157,14 +156,14 @@ QuizChoice.create!(
 QuizChoice.create!(
   content: "パイナップル",
   correct_answer: true,
-  explanation: "このスープはタマリンドが入っており、甘酸っぱい味わいが特徴です。これは「canh chua」と呼ばれる魚介スープです。大体の日本人は苦手だそうです。",
-  image_url: "https://kan-kentei-bucket.s3.ap-northeast-1.amazonaws.com/vietnam/q5_exp.jpg",
+  explanation: "甘酸っぱい味わいの魚介スープで、「canh （スープー）chua（酸っぱい）」とよびます。一部の日本人には好まれない傾向があります。",
+  image_url: nil,
   quiz: quiz5
 )
 
 #---------問題６--------------------------------------------------------------------
 quiz6 = Quiz.create!(
-  content: "⑥今日は10月20日です。散歩をしていたら、あちこちで花が売られています。今日は何の日？",
+  content: "⑥今日は10月20日です。散歩をしていたら、あちこちで花が売られています。今日は何の日でしょう？",
   youtube_url: nil,
   image_url: nil,
   test_category: vietnam
@@ -173,9 +172,8 @@ quiz6 = Quiz.create!(
 QuizChoice.create!(
   content: "女性の日",
   correct_answer: true,
-  explanation: "10月20日はベトナムの女性の日です。職場の女性に花やプレゼントを渡さないと睨まれたり、評価が下がることもあります。\n
-            ベトナムで生活することになったら、この日を忘れないようにしましょう。特に男性は注意が必要です！",
-  image_url: nil,
+  explanation: "⑥10月20日はベトナムの女性の日です。彼女や奥さんにプレゼントをするのはもちろん、職場の女性にもプレゼントを渡さないと評価が下がることもあります（もちろん地域や個人にもよります）。",
+  image_url: "https://kan-kentei-bucket.s3.ap-northeast-1.amazonaws.com/vietnam/q6_exp.jpg",
   quiz: quiz6
 )
 
@@ -197,7 +195,7 @@ QuizChoice.create!(
 
 #---------問題７--------------------------------------------------------------------
 quiz7 = Quiz.create!(
-  content: "⑦今日は徒歩で市内のお土産屋さんを巡ります。バイクが多くて道を渡るタイミングがわかりません。側にいるおじさんが渡るコツを教えてくれました。そのコツとは？",
+  content: "⑦お土産を買いに徒歩で市内のお土産屋さんを巡っています。バイクが多くて道を渡るタイミングがわかりません。側にいるおじさんが渡るコツを教えてくれました。そのコツとは何でしょう？",
   youtube_url: nil,
   image_url: nil,
   test_category: vietnam
@@ -222,15 +220,14 @@ QuizChoice.create!(
 QuizChoice.create!(
   content: "バイクの運転手にアイコンタクトをしながらゆっくり渡る",
   correct_answer: true,
-  explanation: "最初は渡るタイミングが分からないかもしれませんが、2回経験すれば3回目からはスムーズに渡れるようになります。\n
-          走ったり、途中で引き返したりするのは非常に危険ですのでやめましょう。バイクの運転手にこれから渡るというアイコンタクトをしながら、落ち着いて渡るのコツです。不安な方は、他の人の後ろについていくのが安心です。",
-  image_url: nil,
+  explanation: "最初は渡るタイミングが分からないかもしれませんが、慣れればスムーズに渡れるようになります（大体3〜5回経験をすると慣れます）。走ったり、途中で引き返したりするのは非常に危険ですのでやめましょう。バイクの運転手にこれから渡るというアイコンタクトをしながら、落ち着いて渡るのコツです。不安な方は、他の人の後ろについていくのが安心です。",
+  image_url: "https://kan-kentei-bucket.s3.ap-northeast-1.amazonaws.com/vietnam/q7_exp.jpg",
   quiz: quiz7
 )
 
 #---------問題８--------------------------------------------------------------------
 quiz8 = Quiz.create!(
-  content: "⑧旅の締めはやはりビール！という事で、屋台でベトナムのビールを注文しました。ベトナムのビールと言えば？",
+  content: "⑧旅の締めはやはりビール！という事で、屋台でベトナムのビールを注文しました。ベトナムのビールと言えばどれでしょう？",
   youtube_url: nil,
   image_url: nil,
   test_category: vietnam
@@ -239,8 +236,7 @@ quiz8 = Quiz.create!(
 QuizChoice.create!(
   content: "333",
   correct_answer: true,
-  explanation: "このビールは国内シェア率70%以上を誇る人気者です。ブランド名の読み方は「バーバーバー」で、これはベトナム語で数字の「3」を意味します。\n
-            「333」だけでなく他にも多くのベトナムビールがありますので、興味があればぜひ現地でお試しください。日本国内でも購入することが可能です！",
+  explanation: "⑧このビールは国内シェア率70％以上を誇る人気者です。読み方は「バーバーバー」です。ベトナム語では数字の3は「バー」と言います。333以外にもベトナムビールがあるので、興味がある方は現地で探してみてください！",
   image_url: "https://kan-kentei-bucket.s3.ap-northeast-1.amazonaws.com/vietnam/q8_exp.png",
   quiz: quiz8
 )
