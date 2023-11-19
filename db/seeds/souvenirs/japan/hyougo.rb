8d87cd28e25eafde962c3d4b040a88855ce15dbf
Kieu <bsb20131417@gmail.com>
2023-11-19 22:34:09 +0900

    Add new directory to separate categories

---
+++ db/seeds/souvenirs/japan/hyougo.rb
@@ -0,0 +1,36 @@
+hyougo = TestCategory.find_by(title: "兵庫")
+
+gift1 = SouvenirPhoto.create!(
+  name: "明石焼き", 
+  explanation: nil,
+  image_url: "https://kan-kentei-bucket.s3.ap-northeast-1.amazonaws.com/hyougo/q3_exp.jpg",
+  test_category: hyougo
+)
+
+gift1 = SouvenirPhoto.create!(
+  name: "たこせんべい", 
+  explanation: nil,
+  image_url: nil,
+  test_category: hyougo
+)
+
+gift1 = SouvenirPhoto.create!(
+  name: "神戸プリン", 
+  explanation: nil,
+  image_url: "https://kan-kentei-bucket.s3.ap-northeast-1.amazonaws.com/hyougo/puddin_sou.jpg",
+  test_category: hyougo
+)
+
+gift1 = SouvenirPhoto.create!(
+  name: "神戸牛", 
+  explanation: nil,
+  image_url: "https://kan-kentei-bucket.s3.ap-northeast-1.amazonaws.com/hyougo/q1_exp.jpg",
+  test_category: hyougo
+)
+
+gift1 = SouvenirPhoto.create!(
+  name: "明石のり", 
+  explanation: nil,
+  image_url: "https://kan-kentei-bucket.s3.ap-northeast-1.amazonaws.com/hyougo/seaweed_sou.jpg",
+  test_category: hyougo
+)
