c31c1fd10e285fb8de34e942b9b87b4c375d968d
Kieu <bsb20131417@gmail.com>
2023-11-21 21:50:50 +0900

    Add conditional branching to avoid creating duplicate reviews

--- db/seeds/reviews/review.rb
+++ db/seeds/reviews/review.rb
@@ -1,5 +1,7 @@
 TestCategory.all.each do |test_category|
-  Review.create!(test_category_id: test_category.id, content: "①検定は難しかったですか？")
-  Review.create!(test_category_id: test_category.id, content: "②同じ級で違う問題もチャレンジしてみたいですか？")
-  Review.create!(test_category_id: test_category.id, content: "③合格した級に行ってみたいですか？")
+  if Review.where(test_category_id: test_category.id).empty?
+    Review.create!(test_category_id: test_category.id, content: "①検定は難しかったですか？")
+    Review.create!(test_category_id: test_category.id, content: "②同じ級で違う問題にもチャレンジしてみたいですか？")
+    Review.create!(test_category_id: test_category.id, content: "③合格した級に行ってみたいですか？")
+  end
 end
