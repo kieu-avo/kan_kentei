0be89b5a74678426e20b27a8b0c4e06c9ce0d9b5
Kieu <bsb20131417@gmail.com>
2023-11-22 12:03:13 +0900

    Remove comment

--- db/seeds/reviews/review.rb
+++ db/seeds/reviews/review.rb
@@ -1,7 +1,7 @@
 TestCategory.all.each do |test_category|
-  #if Review.where(test_category_id: test_category.id).empty?
+  if Review.where(test_category_id: test_category.id).empty?
     Review.create!(test_category_id: test_category.id, content: "①検定は難しかったですか？")
     Review.create!(test_category_id: test_category.id, content: "②同じ級で違う問題にもチャレンジしてみたいですか？")
     Review.create!(test_category_id: test_category.id, content: "③合格した級に行ってみたいですか？")
-  #end
+  end
 end
