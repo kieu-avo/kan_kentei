550741f4fe728b6aa98af56e7fca30b403643d05
Kieu <bsb20131417@gmail.com>
2023-11-23 11:40:45 +0900

    Update code using rubocop

--- app/models/souvenir_photo.rb
+++ app/models/souvenir_photo.rb
@@ -2,7 +2,7 @@ class SouvenirPhoto < ApplicationRecord
   belongs_to :test_category
   has_many :test_comments, dependent: :destroy
 
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "explanation", "id", "image_url", "name", "test_category_id", "updated_at"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at explanation id image_url name test_category_id updated_at]
   end
 end
