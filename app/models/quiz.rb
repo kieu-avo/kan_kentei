00f7a75cbf018c851c3b7769184d9be8691bc4ae
Kieu <bsb20131417@gmail.com>
2023-11-23 11:38:41 +0900

    Update code using rubocop

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,7 +3,7 @@ class Quiz < ApplicationRecord
   has_many :quiz_choices, dependent: :destroy
   has_many :user_quiz_answers, dependent: :destroy
 
-  #特定のカテゴリーのクイズ数をカウント
+  # 特定のカテゴリーのクイズ数をカウント
   def self.total_by_category(category_id)
     category = TestCategory.find_by(id: category_id)
     return 0 unless category
