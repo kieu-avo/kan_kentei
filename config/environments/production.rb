2309ee5e472f1856822f219477b0dcf04b489307
Kieu <bsb20131417@gmail.com>
2023-12-03 09:56:52 +0900

    Change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'kan-kentei-be9cd1cb33fd.herokuapp.com',
+    domain:               'gmail.com',
     user_name:             ENV['GMAIL_USER_NAME'],
     password:              ENV['GMAIL_PASSWORD'],
     authentication:        'plain',
