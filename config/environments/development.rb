fa45441ff721d88f333c32604a0f2654d4f7a566
Kieu <bsb20131417@gmail.com>
2023-12-05 22:01:22 +0900

    Add config.hosts to use ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  #Line Messagingの確認をするため
+  config.hosts << '.ngrok.io'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
