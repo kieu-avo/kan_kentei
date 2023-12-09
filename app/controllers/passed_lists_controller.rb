e4c1225916e5f6a0d318f843a60d9c1a33d171a1
Kieu <bsb20131417@gmail.com>
2023-12-09 18:48:34 +0900

    Add order method to reflect new order

--- app/controllers/passed_lists_controller.rb
+++ app/controllers/passed_lists_controller.rb
@@ -1,6 +1,6 @@
 class PassedListsController < ApplicationController
   def index
-    @passed_quizzes = current_user.passed_quizzes
+    @passed_quizzes = current_user.passed_quizzes.order(created_at: :desc)
     @japan_quizzes = @passed_quizzes.select { |quiz| quiz.test_category.region == 'japan' }
     @foreign_quizzes = @passed_quizzes.select { |quiz| quiz.test_category.region == 'foreign' }
     @test_comments = current_user.test_comments
