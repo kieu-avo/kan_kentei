2e742e9656c32b74b6016bf3b1ab139365a5eda7
Kieu <bsb20131417@gmail.com>
2023-12-09 11:34:58 +0900

    Update code to separate region in index

--- app/controllers/passed_lists_controller.rb
+++ app/controllers/passed_lists_controller.rb
@@ -1,6 +1,8 @@
 class PassedListsController < ApplicationController
   def index
     @passed_quizzes = current_user.passed_quizzes
+    @japan_quizzes = @passed_quizzes.select { |quiz| quiz.test_category.region == 'japan' }
+    @foreign_quizzes = @passed_quizzes.select { |quiz| quiz.test_category.region == 'foreign' }
     @test_comments = current_user.test_comments
   end
 end
