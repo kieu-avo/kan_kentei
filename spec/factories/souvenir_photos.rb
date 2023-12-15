2f2ccef15432cba774c52a87ef3ed433489c6163
Kieu <bsb20131417@gmail.com>
2023-12-15 16:53:33 +0900

    Update code to show random souvenir

--- spec/factories/souvenir_photos.rb
+++ spec/factories/souvenir_photos.rb
@@ -2,14 +2,6 @@ FactoryBot.define do
   factory :souvenir_photo do
     association :test_category
     
-    name { "ムーミングッズ" }
-    
-    trait :name2 do
-      name { "オーナメント" }
-    end
-
-    trait :name3 do
-      name { "北欧雑貨" }
-    end
+    name { ["ムーミングッズ", "オーナメント", "北欧雑貨"].sample }
   end
 end
